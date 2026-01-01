b2ceb626409e215004582a8c915fb44658c2924e
Hadji Tejuco <hadji.tejuco@gmail.com>
2026-01-01 10:27:21 +0800

    Update main.py

--- Day 1/main.py
+++ Day 1/main.py
@@ -3,4 +3,24 @@ print("Welcome to my first Python app!")
 name = input ("what is your name? ")
 print(f"Hello. {name}!")
 
+# to run open terminal and type in the dir where you have the main.py 
+# python3 main.py
+##########################################
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Your are an adult")
+else:
+    print("You are a minor")
+
+# python3 main.py
+# Enter your age: 18
+# Your are an adult
+
+# python3 main.py
+# Enter your age: 17
+# You are a minor
+
 ##########################################
